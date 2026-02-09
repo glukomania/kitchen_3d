3dfd024fb034c05e006bf8c4468dce5c45e0c81f
Natalia <natalia.shuvaeva@skoumal.net>
2026-02-09 12:57:00 +0100

    Fix widget for Shopify embedding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,6 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  // Prevent Tailwind from resetting global styles on host page
-  corePlugins: {
-    preflight: false, // Disable Tailwind's base reset
-  },
   theme: {
     extend: {}
   },
