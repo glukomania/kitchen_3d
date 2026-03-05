fbd5ec35fb69391ee3b86bb6da8c37e84cd4b7a4
Natalia <natalia.shuvaeva@skoumal.net>
2026-03-05 20:42:13 +0100

    Add console.log for test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ProductsFromUrlStub } from "@/ProductsFromUrlStub";
 // import { Configurator } from "@/features/configurator/ui/Configurator";
 
 export function App() {
+  console.log('App')
   return <ProductsFromUrlStub />;
 }
 
